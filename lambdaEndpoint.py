52009b6ade46d0fdc6b46d2952f98ae00d09bbdb
chansoen <22986484+chsoen@users.noreply.github.com>
2024-11-17 11:09:00 -0800

    Process multiple movie review sentences in the lambda endpoint.

--- lambdaEndpoint.py
+++ lambdaEndpoint.py
@@ -1,18 +1,36 @@
 import json
 
-
 import boto3
+
+SENTIMENT_TYPES = {
+    'POSITIVE',
+    'NEGATIVE',
+    'NEUTRAL',
+    'MIXED'
+}
 
 client = boto3.client('comprehend')
 
 def lambda_handler(event, context):
 
-    text = event['text']
-    sentiment = client.detect_sentiment(Text=text,LanguageCode='en')
-    prediction = sentiment['Sentiment']
-    score = sentiment['SentimentScore']
+    textlist = event['textlist']
+    sentimentDict = client.batch_detect_sentiment(
+        TextList=textlist,
+        LanguageCode='en'
+        )
     
-    return {
-        'comprehend_prediction': prediction,
-        'comprehend_scores': score
-    }
+    resultList = sentimentDict['ResultList']
+    for sentiment in resultList:
+        prediction = sentiment['Sentiment']
+        score = sentiment['SentimentScore']
+        print(prediction)
+        print(score)
+    
+    
+    sentimentCounts = {}
+    for sentiment_type in SENTIMENT_TYPES:
+        sentimentsOfType = list(filter(lambda sentiment: sentiment['Sentiment'] == 'POSITIVE', resultList))
+        sentimentCounts[sentiment_type] = len(sentimentsOfType)
+
+    print(sentimentCounts)
+    return sentimentCounts
