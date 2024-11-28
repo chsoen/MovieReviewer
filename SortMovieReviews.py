ef3f40df4fcdf402d70dd58eb61019c6a5aaff3d
chansoen <22986484+chsoen@users.noreply.github.com>
2024-11-27 21:27:16 -0800

    Able to write to DynamoDB on an S3 object create event.

--- SortMovieReviews.py
+++ SortMovieReviews.py
@@ -3,21 +3,25 @@ import urllib.parse
 import boto3
 
 s3 = boto3.client('s3')
+dynamodb = boto3.client('dynamodb')
 
 def lambda_handler(event, context):
 
-    # bucket = event['Records'][0]['s3']['bucket']['name']
-    # key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
-    bucket = 'raw-reviews'
-    key = 'test.json'
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
     response = s3.get_object(Bucket=bucket, Key=key)
     file = response['Body'].read().decode('utf-8')
     reviews = json.loads(file)
-    print("Showing response under")
-    print(reviews)
-    print("End of response")
+    for review in reviews:
+        dynamodb.put_item(
+            TableName='MovieReviews',
+            Item={
+                'movie':{'S': review['movie']},
+                'reviewer':{'S': review['reviewer']},
+                'review_detail':{'S': review['review_detail']}
+            }
+        )
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('Successful sorting of new review entries.')
     }
-
