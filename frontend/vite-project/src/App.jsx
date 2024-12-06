e87e3e1a6bd88aa595b6d96db74ca74a0a5b7259
VishalV1807 <vishalvaswani8101@gmail.com>
2024-12-06 15:03:20 -0800

    final pushes

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -8,86 +8,8 @@ import reviewData from '../../../1000samples.json'
 function App() {
   const [count, setCount] = useState("")
   const [pictures, setPictures] = useState([])
-  const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZDc5ZDZjOWIwYWQzMjUwMmMxOWE1MjZiMzE2Mjg3MiIsIm5iZiI6MTczMzI2ODI1NC43NTksInN1YiI6IjY3NGY5MzFlNTIwMWY4YzE1ZjE3Njk0NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UWLIhgxOfpLKalcIjFqno90u59CCz9Gz98gwXR_6Tm4'
-  // useEffect(async () => {
-  //   const promises = []
-  //   reviewData.map(async (review, id) => {
-  //     if(id >= 20) {
-  //       return;
-  //     }
-  //     let movie_name = review["movie"]
-  //     let new_name = movie_name.split(' ')
-  //     new_name.pop()
-  //     let name = new_name.join(' ')
-  //     names.push(name)
 
-  //     const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=Cube%20Zero&include_adult=false&language=en-US&page=1`;
-  //     const options = {
-  //       method: 'GET',
-  //       headers: {
-  //         accept: 'application/json',
-  //         Authorization: `Bearer ${API_KEY}` 
-  //       }
-  //     }
-  //     promises.push(fetch(url, options))
-  //   })
-  //   let responses = await Promise.allSettled(promises)
-  //   responses.forEach(async (response)=>{
-  //     if(response.status === "fulfilled") {
-  //       // console.log(response.value.json());
-        
-  //       let data = await response.value.json()
-  //       pictures.push(data["results"][0]["poster_path"])
-  //     } else {
-  //       console.log(response.value);
-        
-  //     }
-  //   })
-  // }, [])
-  useEffect(()=> {
-    const BASE_URL = `https://api.themoviedb.org/3/search/movie`
-    const url = 'https://api.themoviedb.org/3/discover/movie?with_genres=28';
-    const options = {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZDc5ZDZjOWIwYWQzMjUwMmMxOWE1MjZiMzE2Mjg3MiIsIm5iZiI6MTczMzI2ODI1NC43NTksInN1YiI6IjY3NGY5MzFlNTIwMWY4YzE1ZjE3Njk0NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UWLIhgxOfpLKalcIjFqno90u59CCz9Gz98gwXR_6Tm4'
-      }
-    };
 
-    fetch(url, options)
-      .then(res => res.json())
-      .then(json => {
-        // pictures.push(json["poster_path"])
-        console.log(json);
-        const arr = []
-        const onlyMovieNames = reviewData.map(data=>data["movie"])
-        const movienames = onlyMovieNames.map(title=>title.split("(")[0])
-        console.log(movienames)
-        for(const movie of json['results']) {
-          // console.log(reviewData.map(data=>data["movie"]))
-          console.log(movie["title"])
-          if(movie["title"].includes(movienames)) {
-            console.log("Found");
-            arr.push(movie["poster_path"])
-          }
-        }
-        setPictures([...arr])
-      })
-      .catch(err => console.error(err));
-      }, [])
-  // console.log(movieData[0])
-
-  useEffect(()=>{
-    console.log("called")
-    console.log(pictures);
-    
-  }, [pictures])
-
-  async function getPath(name) {
-
-    return "/cx31RUjyim2Bg0YEzbKZZI3imJ9.jpg"
-  }
   return (
     <>
       <div className=' text-center'>
@@ -101,8 +23,6 @@ function App() {
             return <Card key={idx} name = {movie_name} path={pictures[idx]??""}/>
           })
         }
-        {/* <Card name = {"Thappad (I) (2020)"}/>
-        <Card name = {"After Life (2019\u2013 )"}/> */}
       </div>
     </>
   )
--- frontend/vite-project/src/components/Card.jsx
+++ frontend/vite-project/src/components/Card.jsx
@@ -1,12 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import Star from './Star'
 
 const Card = (props) => {
 
     const [rating, setrating] = useState(-1)
     const [ratingvisible, setRatingVisible] = useState(false)
 
+    const roundRating = (num)=> {
+      return Math.round(num * 2) / 2;
+    }
+
+    useEffect(()=>{
+      if((rating % 0.5) === 0) return
+      setrating(prev=>roundRating(prev))
+    }, [rating])
+
     async function getRating() {
-        console.log(props.name)
         let pre_rating = await fetch(`http://localhost:3000/movies/${props.name}`)
         let final_rating_json = await pre_rating.json()
         setrating(final_rating_json["rating"])
@@ -17,13 +26,17 @@ const Card = (props) => {
     }
 
   return (
-    <div className='w-[300px] border border-solid flex flex-col gap-2'>
+    <div className='w-[300px] border border-solid flex flex-col gap-2 pb-3'>
       <img src={`https://static.vecteezy.com/system/resources/thumbnails/012/657/549/small/illustration-negative-film-reel-roll-tapes-for-movie-cinema-video-logo-vector.jpg`} alt="" className='w-[300px] h-[200px] object-cover'/>
-      {/* <img src={`https://image.tmdb.org/t/p/original${props.path}`} alt="" className='w-[300px] h-[200px] object-cover'/> */}
       <h1 className='font-bold text-center'>{props.name}</h1>
-      {/* props.name?.split("(")[0 */}
       {ratingvisible && <h1 className=' text-center'>Rating: {rating}</h1>}
-      <button onClick={getRating} className='bg-slate-600 rounded-md p-2 mx-auto'>get rating</button>
+      <div className='flex gap-1.25 justify-center text-yellow-300'>
+        {rating >= 0 && Array.from({ length: Math.floor(rating) }).map((_, index) => (
+            <Star isFull={true}/>
+        ))}
+      {(rating - (Math.floor(rating))) > 0 && <Star isFull={false}/>}
+      </div>
+      <button onClick={getRating} className='bg-slate-600 rounded-md p-2 mx-auto text-white'>Get Rating</button>
     </div>
   )
 }
